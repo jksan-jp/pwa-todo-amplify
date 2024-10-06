2b781f7fb9cb1b794e7f2965cc3eff41eeab1c06
jksan <jksan.jp@gmail.com>
2024-10-06 12:13:54 +0900

    amplify push

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "analytics": {
+    "pwatodoamplify": {
+      "Id": "string",
+      "Region": "string",
+      "appName": "string"
+    }
+  },
   "api": {
     "pwatodoamplify": {
       "GraphQLAPIEndpointOutput": "string",
